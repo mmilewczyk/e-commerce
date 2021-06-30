e6ae442890f616258891e5b5059c1a2577e67938
agiklo <54153910+agiklo@users.noreply.github.com>
2021-06-30 23:51:22 +0200

    feat: Create model classes (Category, Price, Product)

---
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/model/Category.java
@@ -0,0 +1,26 @@
+package com.milewczyk.productcatalogservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "CATEGORIES")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
+    private List<Product> products;
+}
---
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/model/Price.java
@@ -0,0 +1,30 @@
+package com.milewczyk.productcatalogservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+
+@Getter
+@Setter
+@Embeddable
+public class Price {
+
+    public static final Currency CURRENCY_USD = Currency.getInstance("USD");
+    public static final Price ZERO = new Price();
+
+    private final Currency currency;
+    private final BigDecimal value;
+
+    private Price(Currency currency, BigDecimal value) {
+        this.currency = currency;
+        this.value = value;
+    }
+
+    protected Price() {
+        this(CURRENCY_USD, BigDecimal.ZERO);
+    }
+}
