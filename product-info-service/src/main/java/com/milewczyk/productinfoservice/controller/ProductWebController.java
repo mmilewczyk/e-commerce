826a46a02598e7013e2e9feb560f57d968192e73
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-09 15:47:57 +0200

    feat: Create ProductWebController with CRUD methods and configuration class to connect database

---
+++ product-info-service/src/main/java/com/milewczyk/productinfoservice/controller/ProductWebController.java
@@ -0,0 +1,39 @@
+package com.milewczyk.productinfoservice.controller;
+
+import com.milewczyk.productinfoservice.model.Product;
+import com.milewczyk.productinfoservice.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping(produces = "application/json", path = "api/web/products")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class ProductWebController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId) {
+        return status(HttpStatus.OK).body(productService.getProductById(productId));
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> editProductContent(@RequestBody Product product){
+        return status(HttpStatus.OK).body(productService.updateProduct(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addNewProduct(@RequestBody Product product) {
+        return status(HttpStatus.CREATED).body(productService.addNewProduct(product));
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProductById(@PathVariable("productId") Long productId){
+        productService.deleteProductById(productId);
+    }
+}
