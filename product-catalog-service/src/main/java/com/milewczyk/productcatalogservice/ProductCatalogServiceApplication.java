e7e242b71a6bad7a799b6862d44b1c2c544b1ee2
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-10 13:20:22 +0200

    feat: create getRestTemplate method which is Bean and LoadBalanced

--- product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/ProductCatalogServiceApplication.java
@@ -3,6 +3,9 @@ package com.milewczyk.productcatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -11,5 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ProductCatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
