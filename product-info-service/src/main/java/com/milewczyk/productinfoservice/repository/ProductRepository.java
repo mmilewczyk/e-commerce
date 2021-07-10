9920c8db45772db2c8f1166a128edda93e8b957a
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-10 13:15:51 +0200

    refactor: wrap product by optional in ProductRepository

--- product-info-service/src/main/java/com/milewczyk/productinfoservice/repository/ProductRepository.java
+++ product-info-service/src/main/java/com/milewczyk/productinfoservice/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.milewczyk.productinfoservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findProductById(Long id);
+    Optional<Product> findProductById(Long id);
 }
