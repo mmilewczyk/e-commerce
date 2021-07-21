50683c7e3ade4666e407157af6b3e178c329553b
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-21 11:28:30 +0200

    feat: create getProductById method and change name of class to CatalogProductWebController

---
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/controller/CatalogProductWebController.java
@@ -0,0 +1,43 @@
+package com.milewczyk.productcatalogservice.controller;
+
+import com.milewczyk.productcatalogservice.model.CatalogProduct;
+import com.milewczyk.productcatalogservice.model.Product;
+import com.milewczyk.productcatalogservice.service.CatalogProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping(produces = "application/json", path = "api/web/products")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class CatalogProductWebController {
+
+    private final CatalogProductService catalogProductService;
+
+    @GetMapping
+    public ResponseEntity<Page<CatalogProduct>> getAllProducts(Pageable pageable) {
+        return status(HttpStatus.OK).body(catalogProductService.getAllProducts(pageable));
+    }
+
+    @GetMapping("/{productId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Product> getProductById(@PathVariable Long productId){
+        return catalogProductService.getCatalogProductById(productId);
+    }
+
+    @GetMapping("/search-by-brand")
+    public ResponseEntity<Page<CatalogProduct>> getAllProductsByBrandName(@RequestParam("brand") String brandName, Pageable pageable) {
+        return status(HttpStatus.OK).body(catalogProductService.getAllProductsByBrand(brandName, pageable));
+    }
+
+    @GetMapping("/search-by-name")
+    public ResponseEntity<Page<CatalogProduct>> getAllProductsByName(@RequestParam("name") String name, Pageable pageable) {
+        return status(HttpStatus.OK).body(catalogProductService.getAllProductsByName(name, pageable));
+    }
+}
