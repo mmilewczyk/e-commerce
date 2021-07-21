3e00951ec4095176ae90a4898f3630491ff1ab27
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-21 11:22:30 +0200

    feat: add Hystrix dependency to pom.xml and add @EnableCircuitBreaker in ProductCatalogServiceApplication

--- product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/ProductCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.milewczyk.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class ProductCatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
