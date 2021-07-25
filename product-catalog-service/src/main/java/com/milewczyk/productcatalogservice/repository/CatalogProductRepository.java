c442f31ad53666fa43fac46c41e14be9813c0515
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-25 09:48:01 +0200

    fix: fix abstract methods in CatalogProductRepository by swapping Products by CatalogProducts in method names and delete @EntityGraph

--- product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/repository/CatalogProductRepository.java
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/repository/CatalogProductRepository.java
@@ -3,7 +3,6 @@ package com.milewczyk.productcatalogservice.repository;
 import com.milewczyk.productcatalogservice.model.CatalogProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +12,8 @@ import javax.transaction.Transactional;
 public interface CatalogProductRepository extends JpaRepository<CatalogProduct, Long> {
 
     @Transactional
-    @EntityGraph(attributePaths = {"price", "categories"})
-    Page<CatalogProduct> findProductsByBrandNameContainingIgnoreCase(String brandName, Pageable pageable);
+    Page<CatalogProduct> findCatalogProductsByBrandNameContainingIgnoreCase(String brandName, Pageable pageable);
 
     @Transactional
-    @EntityGraph(attributePaths = {"price", "categories"})
-    Page<CatalogProduct> findProductsByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<CatalogProduct> findCatalogProductsByNameContainingIgnoreCase(String name, Pageable pageable);
 }
