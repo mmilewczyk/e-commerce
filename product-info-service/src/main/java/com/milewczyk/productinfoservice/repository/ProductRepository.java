f72d7c2e1bd5ca86f940499c94ee4d2a80fe2de0
agiklo <54153910+agiklo@users.noreply.github.com>
2021-07-25 10:06:24 +0200

    feat: add @EntityGraph to optimize queries to database

--- product-info-service/src/main/java/com/milewczyk/productinfoservice/repository/ProductRepository.java
+++ product-info-service/src/main/java/com/milewczyk/productinfoservice/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.milewczyk.productinfoservice.repository;
 
 import com.milewczyk.productinfoservice.model.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,6 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @EntityGraph(attributePaths = {"price", "categories"})
     Optional<Product> findProductById(Long id);
 }
