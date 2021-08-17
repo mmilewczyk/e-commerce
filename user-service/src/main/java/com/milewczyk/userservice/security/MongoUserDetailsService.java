2fd1da4bc48e36c5cfffa1cd2ac1cecd6c5b6eb5
agiklo <54153910+agiklo@users.noreply.github.com>
2021-08-17 12:40:00 +0200

    feat: create MongoUserDetailsService and MyUserDetails

---
+++ user-service/src/main/java/com/milewczyk/userservice/security/MongoUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.milewczyk.userservice.security;
+
+import com.milewczyk.userservice.model.User;
+import com.milewczyk.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MongoUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(userName);
+        if (user.isPresent()) {
+            return user.map(MyUserDetails::new).get();
+        } else {
+            throw new UsernameNotFoundException(userName + " not found");
+        }
+    }
+}
---
+++ user-service/src/main/java/com/milewczyk/userservice/security/MyUserDetails.java
@@ -0,0 +1,64 @@
+package com.milewczyk.userservice.security;
+
+import com.milewczyk.userservice.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyUserDetails implements UserDetails {
+
+    private final String userName;
+    private final String password;
+    private final boolean isActive;
+    private final List<GrantedAuthority> authorities;
+
+    public MyUserDetails(User user) {
+        this.userName = user.getUsername();
+        this.password = user.getPassword();
+        this.isActive = user.isActive();
+        this.authorities = user.getRole()
+                .stream()
+                .map(Enum::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
