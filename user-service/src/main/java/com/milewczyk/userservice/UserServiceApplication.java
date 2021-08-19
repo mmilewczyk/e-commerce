de008eea232eece2634d97eda42eafc6accfd5da
agiklo <54153910+agiklo@users.noreply.github.com>
2021-08-19 16:43:01 +0200

    feat: add swagger and jwt dependencies to pom.xml, enable swagger in UserServiceApplication class

--- user-service/src/main/java/com/milewczyk/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/milewczyk/userservice/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.milewczyk.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class UserServiceApplication {
 
     public static void main(String[] args) {
