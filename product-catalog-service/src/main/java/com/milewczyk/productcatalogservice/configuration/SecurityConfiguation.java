55558214fe590804720e8dd722cb5cae66236833
agiklo <54153910+agiklo@users.noreply.github.com>
2021-08-28 23:35:16 +0200

    feat: use product-catalog-service as ResourceServer by using OAuth2, create simple authorization configuration

---
+++ product-catalog-service/src/main/java/com/milewczyk/productcatalogservice/configuration/SecurityConfiguation.java
@@ -0,0 +1,24 @@
+package com.milewczyk.productcatalogservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguation extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("actuator/**").hasRole(USER_ROLES.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE,  "/admin/products**").hasRole(USER_ROLES.ADMIN.name())
+                .antMatchers("/admin/products**").hasAnyRole(USER_ROLES.ADMIN.name(), USER_ROLES.MODERATOR.name())
+                .antMatchers(HttpMethod.GET, "/web/products**").hasAnyRole(USER_ROLES.USER.name(), USER_ROLES.ADMIN.name(), USER_ROLES.MODERATOR.name())
+                .anyRequest().authenticated();
+    }
+
+    private enum USER_ROLES {
+        USER, ADMIN, MODERATOR
+    }
+}
