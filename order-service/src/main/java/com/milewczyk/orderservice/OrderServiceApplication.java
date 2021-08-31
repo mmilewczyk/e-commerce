e0c10c2e9abc740c4565b4338a48993462d7503f
agiklo <54153910+agiklo@users.noreply.github.com>
2021-08-31 12:26:44 +0200

    feat: create default configuration, create RestTemplate bean

--- order-service/src/main/java/com/milewczyk/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/milewczyk/orderservice/OrderServiceApplication.java
@@ -2,12 +2,29 @@ package com.milewczyk.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableSwagger2
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
