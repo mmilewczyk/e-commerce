4802084e258e65f76a2269abd3c3a11c789ead12
agiklo <54153910+agiklo@users.noreply.github.com>
2021-09-01 19:12:57 +0200

    refactor: comment names of sections in pom.xml, add swagger and actuator to the class path, enable endpoints

--- discovery-service/src/main/java/com/milewczyk/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/milewczyk/discoveryservice/DiscoveryServiceApplication.java
@@ -3,8 +3,10 @@ package com.milewczyk.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 
